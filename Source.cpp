3215eae8821754fd51db0f7233ad3c34c9c9527b
movsb <anhbk@qq.com>
2017-09-06 15:20:09 +0800

    update io types

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,6 @@
 
 static LPFN_ACCEPTEX s_fnAcceptEx;
 static LPFN_GETACCEPTEXSOCKADDRS s_fnGetAcceptExSockAddrs;
-static HANDLE s_hIocp;
 
 static const size_t BUF_SIZE = 64 * 1024 * 1024;
 
@@ -26,75 +25,182 @@ struct OpType
     };
 };
 
-struct PerIOContext
+class WSARet
 {
-    OVERLAPPED _overlapped;
-    SOCKET _sckClient;
-    OpType::Value _op;
-    WSABUF _wsabuf;
-    char _buf[BUF_SIZE];
-
-    PerIOContext()
+public:
+    WSARet(bool b)
+        : _b(b)
     {
-        memset(this, 0, sizeof(*this));
-        _wsabuf.len = _countof(_buf);
-        _wsabuf.buf = &_buf[0];
+        _e = ::WSAGetLastError();
+    }
+
+    bool Succ()     { return _b; }
+    bool Fail()     { return !_b && _e != WSA_IO_PENDING; }
+    bool Async()    { return !_b && _e == WSA_IO_PENDING; }
+    int Code()      { return _e; }
+
+private:
+    bool _b;
+    int _e;
+};
+
+class WSAIntRet : public WSARet
+{
+public:
+    WSAIntRet(int value)
+        : WSARet(value == 0)
+    { }
+};
+
+class WSABoolRet : public WSARet
+{
+public:
+    WSABoolRet(BOOL value)
+        : WSARet(value != FALSE)
+    { }
+};
+
+struct BaseIOContext
+{
+    OVERLAPPED overlapped;
+    OpType::Value optype;
+
+    BaseIOContext(OpType::Value op)
+        : optype(op)
+    {
+        std::memset(&overlapped, 0, sizeof(overlapped));
     }
 };
 
-struct PerSocketContext
+struct InitIOContext : BaseIOContext
 {
-    SOCKET _sckCliet;
-    SOCKADDR_IN _addrClient;
-    SOCKADDR_IN _addrRemote;
-    std::vector<PerIOContext*> _ios;
+    InitIOContext()
+        : BaseIOContext(OpType::Init)
+    { }
+};
+
+struct AcceptIOContext : BaseIOContext
+{
+    AcceptIOContext()
+        : BaseIOContext(OpType::Accept)
+    {
+        fd = ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
+        assert(fd != INVALID_SOCKET);
+    }
+
+    WSARet Accept(SOCKET listen)
+    {
+        DWORD dwBytes;
+
+        WSABoolRet R = s_fnAcceptEx(
+            listen,
+            fd,
+            buf,
+            0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16,
+            &dwBytes,
+            &overlapped
+        );
+
+        return R;
+    }
+
+    void GetAddresses(SOCKADDR_IN* local, SOCKADDR_IN* remote)
+    {
+        int len;
+        SOCKADDR_IN* _local;
+        SOCKADDR_IN* _remote;
+
+        s_fnGetAcceptExSockAddrs(
+            buf,
+            0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16,
+            (sockaddr**)&_local, &len,
+            (sockaddr**)&_remote, &len
+        );
+
+        std::memcpy(local, _local, sizeof(SOCKADDR_IN));
+        std::memcpy(remote, _remote, sizeof(SOCKADDR_IN));
+    }
+
+    SOCKET fd;
+    char buf[(sizeof(SOCKADDR_IN) + 16) * 2];
+};
+
+struct ServerSocketContext
+{
+    SOCKET fd;
+};
+
+struct ClientSocketContext
+{
+    ClientSocketContext(SOCKET fd)
+        : fd(fd)
+    {
+
+    }
+
+    SOCKET fd;
+    SOCKADDR_IN local;
+    SOCKADDR_IN remote;
+};
+
+
+struct ReadIOContext : BaseIOContext
+{
+    ReadIOContext(ClientSocketContext* client)
+        : BaseIOContext(OpType::Read)
+        , client(client)
+    {
+        wsabuf.buf = buf;
+        wsabuf.len = _countof(buf);
+    }
+
+    WSARet Read()
+    {
+        DWORD dwFlags = 0;
+
+        WSAIntRet R = ::WSARecv(
+            client->fd,
+            &wsabuf, 1, nullptr,
+            &dwFlags,
+            &overlapped,
+            nullptr
+        );
+
+        return R;
+    }
+
+    ClientSocketContext* client;
+    WSABUF wsabuf;
+    char buf[BUF_SIZE];
 };
 
 static unsigned int __stdcall worker_thread(void* tag)
 {
-    PerSocketContext* pSocketContext;
-    PerIOContext* pIoContext;
+    HANDLE hIocp = static_cast<HANDLE>(tag);
+    ServerSocketContext* pServerContext;
+    BaseIOContext* pIoContext;
     DWORD dwBytesTransfered;
+
     while(true) {
-        BOOL bRet = GetQueuedCompletionStatus(s_hIocp, &dwBytesTransfered, (PULONG_PTR)&pSocketContext, (LPOVERLAPPED*)&pIoContext, INFINITE);
+        BOOL bRet = GetQueuedCompletionStatus(hIocp, &dwBytesTransfered, (PULONG_PTR)&pServerContext, (LPOVERLAPPED*)&pIoContext, INFINITE);
         assert(bRet != FALSE);
 
-        if(pIoContext->_op == OpType::Accept) {
-            auto ctx = new PerSocketContext();
-            int len;
-            SOCKADDR_IN* pClientAddr;
-            SOCKADDR_IN* pRemoteAddr;
-            s_fnGetAcceptExSockAddrs(pIoContext->_buf,
-                0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16, 
-                (sockaddr**)&pClientAddr, &len, (sockaddr**)&pRemoteAddr, &len);
-            memcpy(&ctx->_addrClient, pClientAddr, sizeof(SOCKADDR_IN));
-            memcpy(&ctx->_addrRemote, pRemoteAddr, sizeof(SOCKADDR_IN));
-
-            auto io = new PerIOContext();
-            io->_op = OpType::Read;
-            int ret = WSARecv(pIoContext->_sckClient, &io->_wsabuf, 1, nullptr, &dwBytesTransfered, &io->_overlapped, nullptr);
-            ret = ret;
+        if(pIoContext->optype == OpType::Accept) {
+            auto acceptio = static_cast<AcceptIOContext*>(pIoContext);
+            auto client = new ClientSocketContext(acceptio->fd);
+            acceptio->GetAddresses(&client->local, &client->remote);
+            auto readio = new ReadIOContext(client);
+            auto ret = readio->Read();
+            assert(ret.Async());
         }
-        else if(pIoContext->_op == OpType::Read) {
+        else if(pIoContext->optype == OpType::Read) {
 
         }
-        else if(pIoContext->_op == OpType::Write) {
-
-        
-        }
-        else if(pIoContext->_op == OpType::Init) {
-            auto io = new PerIOContext();
-            io->_op = OpType::Accept;
-            io->_sckClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-            assert(io->_sckClient != INVALID_SOCKET);
-            DWORD dwBytes;
-            BOOL bRet = s_fnAcceptEx(
-                pSocketContext->_sckCliet,
-                io->_sckClient, &io->_buf,
-                0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16,
-                &dwBytes,
-                &io->_overlapped
-            );
+        else if(pIoContext->optype == OpType::Init) {
+            auto initio = static_cast<InitIOContext*>(pIoContext);
+            auto acceptio = new AcceptIOContext();
+            auto ret = acceptio->Accept(pServerContext->fd);
+            assert(ret.Async());
         }
     }
 }
@@ -103,7 +209,6 @@ int main()
 {
     HANDLE hIocp = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
     assert(hIocp != nullptr);
-    s_hIocp = hIocp;
 
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -111,9 +216,10 @@ int main()
     SOCKET sckServer = WSASocket(AF_INET, SOCK_STREAM, 0, nullptr, 0, WSA_FLAG_OVERLAPPED);
     assert(sckServer != INVALID_SOCKET);
 
-    auto ctx = new PerSocketContext();
-    ctx->_sckCliet = sckServer;
-    ::CreateIoCompletionPort((HANDLE)sckServer, hIocp, (ULONG_PTR)ctx, 0);
+    auto ctx = new ServerSocketContext();
+    ctx->fd = sckServer;
+    hIocp = ::CreateIoCompletionPort((HANDLE)sckServer, hIocp, (ULONG_PTR)ctx, 0);
+    assert(hIocp != nullptr);
 
     SOCKADDR_IN addrServer = {0};
     addrServer.sin_family = AF_INET;
@@ -153,13 +259,12 @@ int main()
     assert(s_fnGetAcceptExSockAddrs != nullptr);
 
 
-    for(auto i = 0; i < 10; i++) {
-        _beginthreadex(nullptr, 0, worker_thread, nullptr, 0, nullptr);
+    for(auto i = 0; i < 1; i++) {
+        _beginthreadex(nullptr, 0, worker_thread, (void*)hIocp, 0, nullptr);
     }
 
-    auto io = new PerIOContext();
-    io->_op = OpType::Init;
-    PostQueuedCompletionStatus(hIocp, 0, (ULONG_PTR)ctx, &io->_overlapped);
+    auto io = new InitIOContext();
+    PostQueuedCompletionStatus(hIocp, 0, (ULONG_PTR)ctx, &io->overlapped);
 
     Sleep(INFINITE);
 }
