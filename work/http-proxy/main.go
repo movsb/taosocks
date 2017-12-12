41f81f00eb84d1bf98a78038631e85d5c2c9e8fa
movsb <anhbk@qq.com>
2017-12-12 16:22:37 +0800

    http proxy simplify

--- work/http-proxy/main.go
+++ work/http-proxy/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-    "crypto/tls"
-    "flag"
     "io"
     "log"
     "net"
@@ -57,18 +55,6 @@ func copyHeader(dst, src http.Header) {
 }
 
 func main() {
-    var pemPath string
-    flag.StringVar(&pemPath, "pem", "server.pem", "path to pem file")
-    var keyPath string
-    flag.StringVar(&keyPath, "key", "server.key", "path to key file")
-    var proto string
-    flag.StringVar(&proto, "proto", "https", "Proxy protocol (http or https)")
-    flag.Parse()
-
-    if proto != "http" && proto != "https" {
-        log.Fatal("Protocol must be either http or https")
-    }
-
     server := &http.Server{
         Addr: ":8888",
         Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -78,14 +64,8 @@ func main() {
                 handleHTTP(w, r)
             }
         }),
-        // Disable HTTP/2.
-        TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
     }
 
-    if proto == "http" {
-        log.Fatal(server.ListenAndServe())
-    } else {
-        log.Fatal(server.ListenAndServeTLS(pemPath, keyPath))
-    }
+    log.Fatal(server.ListenAndServe())
 }
 
